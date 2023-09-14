ce53a0b3ed1e8965e12dd2819f3fe29cb99d0627
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-14 16:06:35 +0200

    updated search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import "./App.css"
 import Title from "./components/Title";
 import movieListData from "../film.js";
 import SearchBar from "./components/SearchBar";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState(""); 
+  const filteredMovies = movieListData.filter((movie) =>
+    movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearchChange = (search) => {
+    setSearchTerm(search);
+  };
+
   return (
     <div className="main-container">
-      <Title/>
-      <SearchBar/>
-      <MovieList movies={movieListData} />
+      <Title />
+      <SearchBar onSearchChange={handleSearchChange} />
+      <MovieList movies={filteredMovies} />
     </div>
   );
 };
+
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,18 @@
-const SearchBar = () => {
-    return (
-        <input placeholder="Make your search..." type="text"></input>
-    )
-}
+const SearchBar = ({ onSearchChange }) => {
+  const handleOnChange = (e) => {
+    const userInput = e.target.value;
+    onSearchChange(userInput); 
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Make your search..."
+        type="text"
+        onChange={handleOnChange}
+      ></input>
+    </div>
+  );
+};
 
 export default SearchBar;
