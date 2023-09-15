90caa08fa74e2ad1ab64a720ae2eb2a1fe79bcd8
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-15 16:45:25 +0200

    conditional render added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import MovieList from "./components/MovieList"; 
-import "./App.css"
+import MovieList from "./components/MovieList";
+import "./App.css";
 import Title from "./components/Title";
 import movieListData from "../film.js";
 import SearchBar from "./components/SearchBar";
 import { useState } from "react";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
 
-  //Function to search for the specific movie
+  // Function to search for the specific movie
   const handleSearchChange = (search) => {
     setSearchTerm(search);
   };
-    
-  //Function to filter the movies 
-    const filteredMovies = movieListData.filter((movie) =>
-    movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+
+  // Conditionally filter the movies or show all movies
+  const filteredMovies = searchTerm
+    ? movieListData.filter((movie) =>
+        movie.Title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : movieListData;
 
   return (
     <div className="main-container">
@@ -26,6 +28,5 @@ const App = () => {
     </div>
   );
 };
-
 
 export default App;
