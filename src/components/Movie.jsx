b7fc660c6eddc062433459bd6e32b55e6daf38b7
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-15 16:39:26 +0200

    heart styling added

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -9,11 +9,13 @@ const Movie = ({movie}) => {
     <div className="movie-card">
     <img src={movie.Poster} alt={movie.Title} />
     <h2>{movie.Title}</h2>
+    <div className="year-likes-wrapper">
     <p>Year: {movie.Year}</p>
     <p className={`heart-icon ${like ? 'liked' : 'not-liked'}`}
         onClick={handleLike}>
         ❤
     </p>
+    </div>
   </div>
     )
 }
